1361df83c9c38cc0382d52d69e550f1926fb7809
carloscapel <75810131+carloscapel@users.noreply.github.com>
2021-01-22 13:12:58 +0100

    Update gettranslate.py
    Version funcionando. traduce solo el campo TranslatedText

--- todos/gettranslate.py
+++ todos/gettranslate.py
@@ -9,46 +9,63 @@ dynamodb = boto3.resource('dynamodb')
 def gettranslate(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
-    # fetch todo from the database
+      # fetch todo from the database
     result1 = table.get_item(
         Key={
             'id': event['pathParameters']['id']
         }
     )
+    
+    initialdoc = result1
 
     # translate -------------------------------
     #1 - Recover text to translate
-    print  "1 - Recover text to translate"
-    print   "result1 : " + result1
+    print('1 - Recover text to translate')
+    print('result1 : ' + str(result1['Item']))
     
     texto = ''
-    json_object = json.loads(result1)
+    result1 = str(result1['Item']).replace("\'", "\"")
+ 
+    result1 = str(result1).replace('True','"True"')
+    result1 = str(result1).replace('False','False"')
+    result1 = str(result1).replace('Decimal("','"Decimal(\'')
+    result1 = str(result1).replace('")}','\')"}')
+ 
+    print('result1 escapado y parseado: ' + str(result1))
+     
+    json_object = json.loads(str(result1))
     pairs = json_object.items()
 
     for key, value in pairs:
-       if (key == 'text' ) texto = value
+       if key == 'text' : texto = value
     
-    if texto == ''  
-        print("NO ha encontrado el campo text en el registro desde BBDD.")
+    if texto == '':  
+        print('NO ha encontrado el campo text en el registro desde BBDD.')
         return
     
     #2 - translate text
-    translate = boto3.client(service_name='translate', region_name='region', use_ssl=True)
-    if ( event['pathParameters']['language'] = 'FR')
-        result1 = translate.translate_text(Text=texto, SourceLanguageCode="en", TargetLanguageCode="fr")
-    else
-        result1 = translate.translate_text(Text=texto, SourceLanguageCode="en", TargetLanguageCode="es")
-    print("result1 : " + result1)
+    
+    print('texto a traducir : ' + str(texto))
+    
+    textotraducido =''
+    translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
+    if  event['pathParameters']['language'] == 'FR':
+        textotraducido = translate.translate_text(Text=texto, SourceLanguageCode="en", TargetLanguageCode="fr")
+    else :
+        textotraducido = translate.translate_text(Text=texto, SourceLanguageCode="en", TargetLanguageCode="es")
+    
+    print('texto traducido : ' + str(textotraducido))
         
     #3 - change language 
-    result = string.replace(texto, result1)
-    print("result : " + result)
+    
+    result = str(initialdoc).replace(str(texto), str(textotraducido))
+    print('result : ' + str(result))
         
         
     # create a response
     response = {
         "statusCode": 200,
-        "body": json.dumps(result['Item'],
+        "body": json.dumps(result,
                            cls=decimalencoder.DecimalEncoder)
     }
 
